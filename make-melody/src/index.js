1307d3c08e795cc4b925c1ae8046ecb5f6d3e08f
DESKTOP-KVCQHCD\SSAFY <skadnr0621@gmail.com>
2022-04-04 17:17:55 +0900

    Feat: add random music download

--- make-melody/src/index.js
+++ make-melody/src/index.js
@@ -2,117 +2,126 @@ import abcjs from "abcjs";
 import "./style.css";
 import 'abcjs/abcjs-audio.css';
 
-// let str1 = "";
-// let str2 = "";
-// let str3 = "";
-// let example2 = ["C","D","E","F","G","A","B","C,","D,"];
-// //"E,","F,","G,","A,","B,","C","D","E","F","G","A","B","c","d","e","f","g","a","b","c'","d'","e'","f'","g'","a'","b'"
-// var myText = document.getElementById('editor');
-// myText.value = ('X: 1 \n' + 'T: setting \n' + 'M: 4/4 \n' + 'L: 1/8 \n' + 'K: Emin \n' + '|:' + str3 + '|' + str2 + '|'); 
-
-// var mkmd = document.getElementById('gacha');
-// mkmd.addEventListener('click', function(){
-//     for(var i=0; i<4; ++i){
-//       str2+=example2[Math.floor(Math.random()*example2.length)];
-//     }
-//     str3 = example2[Math.floor(Math.random()*example2.length)];
-// });
-
-// //지금 읽히지 못해서 쓰이고 있지 못 하는듯? 쓰이는 식으로 바꿔야합니다.
-// let editor = new abcjs.Editor(editArea, editorParams);
-// window.onload = function(){
-//   abc_editor = new abcjs.Editor("editor", {
-//     canvas_id: "paper",
-//     warnings_id: "warning",
-//     synth: {
-//       el: "#controls",
-//       options: {
-//         displayLoop: true,
-//         displayRestart: true,
-//         displayPlay: true,
-//         displayProgress: true,
-//         displayWarp: true
-//       }
-//     }
-//   });
-// }
-            
-// var cb = document.getElementById('connect_back');
-// cb.addEventListener('click', function(){
-//   str2+=str1;
-//       });
-
-// var cf = document.getElementById('connect_front');
-// cf.addEventListener('click', function(){
-//   str2=str1+str2;
-//       });
-
-// var fm = document.getElementById('flush_melody');
-// fm.addEventListener('click', function(){
-//         str3="";
-//         str2="";
-//       });=
-
-// var cm = document.getElementById('check_melody');
-// cm.addEventListener('click', function(){
-//         alert('Hello check_melody');
-//         //배열 선언해서 그 배열안에 있는 것들 확인하기 
-//       });
-                 
-
-
-
-
-// 잘 안되는 코드. 왜 안되는지 모르겠음. 연구중 : https://paulrosen.github.io/abcjs/visual/overview.html#multiple-tunes
-// 4/4 되""는데? 한번에 되네..
-let example2 = ["C","D","E","F","G","A","B","C,","D,"];
-// "E,","F,","G,","A,","B,","C","D","E","F","G","A","B","c","d","e","f","g","a","b","c'","d'","e'","f'","g'","a'","b'"
-
-
-
-// var str = "a";
-// var abcString = "X:1\nT:Example\nK:Bb\nBcff"+str+"|\n";
+let example2 = ["C","D","E","F","G","A","B","C,","D,","E,","F,","G,","A,","B,","C","D","E","F","G","A","B","c","d","e","f","g","a","b","c'","d'","e'","f'","g'","a'","b'"];
 var abcString="";
 
-var mkmd = document.getElementById('gacha');
-mkmd.addEventListener('click', function(){
-    for(var i=0; i<4; ++i){
-      abcString+=example2[Math.floor(Math.random()*example2.length)];
-    }
-    // str3 = example2[Math.floor(Math.random()*example2.length)];
-    abcjs.renderAbc("paper", abcString);
+var synth = new abcjs.synth.CreateSynth();
+// var synthControl = new ABCJS.synth.SynthController();
 
+var mkmd = document.getElementById('gacha');
+
+var audioParams = { chordsOff: true };
+var CursorControl = function () {
+  this.beatSubdivisions = 2;
+  this.onStart = function () {
+    console.log("The tune has started playing.");
+  };
+  this.onFinished = function () {
+    console.log("The tune has stopped playing.");
+  };
+  this.onBeat = function (beatNumber) {
+    console.log("Beat " + beatNumber + " is happening.");
+  };
+  this.onEvent = function (event) {
+    console.log("An event is happening", event);
+  };
+};
+var cursorControl = new CursorControl();
+mkmd.addEventListener("click", function () {
+  abcString = "";
+  for (var i = 0; i < 16; ++i) {
+    abcString += example2[Math.floor(Math.random() * example2.length)];
+  }
+
+  if (abcjs.synth.supportsAudio()) {
+    var synthControl = new abcjs.synth.SynthController();
+    synthControl.load("#audio", cursorControl, {
+      displayLoop: true,
+      displayRestart: true,
+      displayPlay: true,
+      displayProgress: true,
+      displayWarp: true,
+    });
+
+    var visualObj = abcjs.renderAbc("paper", abcString);
+    var synth = new abcjs.synth.CreateSynth();
+
+    synth
+      .init({
+        visualObj: visualObj[0],
+      })
+      .then(function () {
+        synthControl
+          .setTune(visualObj[0], false, audioParams)
+          .then(function () {
+            console.log("audio successfully loaded");
+            synth.prime().then(function () {
+              var a = document.createElement("a");
+              document.body.appendChild(a);
+              a.style = "display: none";
+
+              var url = synth.download();
+              a.href = url;
+              a.download = abcString + "-fwr-recording.wav";
+              a.click();
+              window.URL.revokeObjectURL(url);
+              document.body.removeChild(a);
+            });
+          })
+          .catch(function (error) {
+            console.warn("Audio problem: ", error);
+          });
+      })
+      .catch(function (reason) {
+        console.log(reason);
+      });
+  }
 });
 
+//뒤에 연결
 var cb = document.getElementById('connect_back');
 cb.addEventListener('click', function(){
-  alert("뒤에 연결");
-      });
+  for(var i=0; i<16; ++i){
+    abcString+=example2[Math.floor(Math.random()*example2.length)];
+  }
+  abcjs.renderAbc("paper", abcString);
+});
 
+//앞에 연결
 var cf = document.getElementById('connect_front');
 cf.addEventListener('click', function(){
-  alert("앞에 연결");
-      });
+  for(var i=0; i<4; ++i){
+    abcString=example2[Math.floor(Math.random()*example2.length)]+abcString;
+  }
+  abcjs.renderAbc("paper", abcString);
+});
 
+//보관함 비우기
 var fm = document.getElementById('flush_melody');
 fm.addEventListener('click', function(){
   alert("보관 비우기");
   abcString="";
   abcjs.renderAbc("paper", abcString);
-      });
+});
 
+//보관함 확인
 var cm = document.getElementById('check_melody');
 cm.addEventListener('click', function(){
-        alert('Hello check_melody');
-        //배열 선언해서 그 배열안에 있는 것들 확인하기 
-      });
+  //보유 멜로디 확인, abcString에 현재 누른 상황에서 갖고 있는 멜로디가 저장됨을 알 수 있음. 
+  alert(abcString);
+});
 
+//다운로드 -> 자동화 완료
 var dm = document.getElementById('download_melody');
 dm.addEventListener('click', function(){
-      alert('file download');
-      });
+  //다운로드 멜로디
+  alert('file download');
+});
 
+//전체 멜로디 확인
 var am = document.getElementById('all_melody');
 am.addEventListener('click', function(){
-  alert('all download');
+  //순열로 만들어진 전체 멜로디 확인하기
+  //순열이 아직 안됨. 다운로드 구현하고 빨리 만들 것. 
+  alert('all melody chk');
 });
